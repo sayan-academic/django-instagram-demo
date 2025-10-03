22c8dd9a71387eb94850ebf624e196f6841d796e
sayan-academic <sayanacademic69@gmail.com>
2025-10-03 23:45:55 +0530

    added whitenoise and requirements.txt

--- instagram/settings.py
+++ instagram/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'whitenoise',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
