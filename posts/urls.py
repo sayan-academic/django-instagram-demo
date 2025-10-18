137a6526191f0a3fb727ef208cf76e10730ecad4
sayan-academic <sayanacademic69@gmail.com>
2025-10-19 03:39:54 +0530

    urls changes

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-#added lines
+
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
